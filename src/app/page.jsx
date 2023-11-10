791353b5da83ac1012aac0fb7f0e27e7b816a21e
mardomai <mardo.mai@ametikool.ee>
2023-11-10 11:41:59 +0200

    yes

---
+++ src/app/page.jsx
@@ -0,0 +1,16 @@
+import NavBar from "./components/burgermenu"
+
+const app = () => {
+  return (<>
+     <div>
+      <NavBar></NavBar>
+    </div>
+  
+    <div>
+    
+    </div>
+  </>
+   );
+}
+ 
+export default app;
