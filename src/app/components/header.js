c0e48556007de088594abbfbf4811082859b885b
mardomai <mardo.mai@ametikool.ee>
2023-12-30 17:26:17 +0200

    yes

--- src/app/components/header.js
+++ src/app/components/header.js
@@ -10,7 +10,7 @@ const Header = () => {
   return (
     <header className="flex justify-between px-5 py-2 bg-primary text-white fixed w-full z-10">
       <a href="/" className="logo text-2xl font-bold text-accent">
-        Amrin
+        Welcome
       </a>
 
       {/* Desktop Nav */}
